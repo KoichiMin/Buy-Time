532be14f9d61e3c26e3348cc4969bf3e51914df7
Yu-Qing-Liu <yu.qing.liu@mail.mcgill.ca>
2022-08-18 17:46:18 -0400

    Setup

---
+++ backend/batchImport.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
+const {Mongo_URI} = process.env;
+
+const options = {
+    
+}
+
+const companies = require('./data/companies.json');
+const items = require('./data/items.json');
+
+const batchImportItems = async () => {
+    const client = new MongoClient(Mongo_URI, options);
+    await client.connect();
+    const db = client.db("Items");
+    await db.collection("ItemsData").insertMany(items);
+    client.close();
+}
+
+const batchImportCompanies = async () => {
+    const client = new MongoClient(Mongo_URI, options);
+    await client.connect();
+    const db = client.db("Companies");
+    await db.collection("CompaniesData").insertMany(companies);
+    client.close();
+}
+
+batchImportItems();
+batchImportCompanies();
+
