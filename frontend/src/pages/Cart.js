b0c5e490943f61902110f2dabf7d764a012ce6a5
Sarah Giulietti <sarahgiulietti@hotmail.com>
2022-08-22 18:51:47 -0400

    cart.js uncomment

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 import CheckOutButton from "../components/Cartpage/CheckOutButton";
 import CartContainer from "../components/Cartpage/CartContainer";
-//import EmptyCartButton from "../components/Cartpage/EmptyCartButton";
+import EmptyCartButton from "../components/Cartpage/EmptyCartButton";
 
 const Cart = () => {
     return(
         <>
         <CartContainer/>
         <CheckOutButton></CheckOutButton>
-        {/* <EmptyCartButton></EmptyCartButton> */}
+        <EmptyCartButton></EmptyCartButton>
         </>
     );
 };
