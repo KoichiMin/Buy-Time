7ef9ed78f71d1176f50c4d2f36dd26bffb1c45c1
Sarah Giulietti <sarahgiulietti@hotmail.com>
2022-08-23 17:59:02 -0400

    buynow btn styling

--- frontend/src/components/Homepage/BuyNowModal.js
+++ frontend/src/components/Homepage/BuyNowModal.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import styled, {GlobalStyleComponent} from "styled-components";
+import styled from "styled-components";
 import Modal from "react-modal";
 
 const customStyles = {
@@ -17,14 +17,8 @@ const BuyNowModal = ({ object }) => {
     const [modalIsOpen, setIsOpen] = useState(false)
     const [confirmed, setConfirmed] = useState(false)
 
-    let subtitle;
-
     const openModal = () => {
         setIsOpen(true)
-    }
-
-    const afterOpenModal = () => {
-        // subtitle.style.color = "#EEE"
     }
 
     const closeModal = () => {
@@ -34,7 +28,6 @@ const BuyNowModal = ({ object }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        e.stopPropagation();
         const _id = object._id
         console.log(_id)
 
@@ -54,47 +47,59 @@ const BuyNowModal = ({ object }) => {
 
     return(
         <>
-            <button onClick={openModal}>buy now</button>
+            <StyledButton onClick={openModal}>buy now</StyledButton>
             <Modal 
             isOpen={modalIsOpen}
-            onAfterOpen={afterOpenModal}
             onRequestClose={closeModal}
             style={customStyles}
             contentLabel="Buy Now Modal"
             >
             {confirmed === false ?
-            <>
-                <p>Please enter your details to complete the purchase.</p>
-                <p>Total: {object.price}</p>
-                <button onClick={closeModal}>close</button>
-                <form onSubmit={handleSubmit}>
-                    <input 
-                    type="text"
-                    placeholder="Full name"
-                    required={true}
-                    />
-                    <input 
-                    type="email"
-                    placeholder="Email"
-                    required={true}
-                    />
-                    <input 
-                    type="text"
-                    placeholder="Address"
-                    required={true}
-                    />
-                    <input 
-                    type="text"
-                    placeholder="Credit card number"
-                    required={true}
-                    />
-                    <button type="submit">Purchase</button>
-                </form>
-            </>
+            
+            <Wrapper>
+                <StyledTitle>Payment</StyledTitle>
+                    <InputContainer>
+                    <h1>Total: {object.price}</h1>
+                    </InputContainer>
+                    <form onSubmit={handleSubmit}>
+
+                    <InputContainer>
+                        <StyledLabel>Card Holder Name</StyledLabel>
+                        <StyledInput required={true} type="text" placeholder="Card Holder Name"></StyledInput>
+                    </InputContainer>
+
+                    <InputContainer>
+                        <StyledLabel>Card Number</StyledLabel>
+                        <StyledInput required={true} type="text" placeholder="Card Number"></StyledInput>
+                    </InputContainer>
+
+                    <InputContainer2>
+
+                        <InputContainer>
+                            <StyledLabel>Expiry Date</StyledLabel>
+                            <StyledInput1 required={true} type="text" placeholder="Expiry Date"></StyledInput1>
+                        </InputContainer>
+
+                        <InputContainer>
+                            <StyledLabel>CVV Code</StyledLabel>
+                            <StyledInput1 required={true} type="text" placeholder="CVV Code"></StyledInput1>
+                        </InputContainer>
+
+                    </InputContainer2>
+
+                    <ButtonContainer>
+                        <StyledConfirmButton type="submit">Confirm</StyledConfirmButton>
+                        <StyledCancelButton onClick={closeModal}>Cancel</StyledCancelButton>
+                    </ButtonContainer>
+
+                    </form>
+            </Wrapper>
                 : confirmed === true &&
                 <>
-                <p>Thank you for your purchase!</p>
-                <button onClick={closeModal}>close</button>
+                <ConfirmedDiv>
+                    <StyledLabel>Thank you for your purchase!</StyledLabel>
+                    <StyledCloseButton onClick={closeModal}>close</StyledCloseButton>
+                </ConfirmedDiv>
                 </>
                 }
             </Modal>
@@ -102,4 +107,123 @@ const BuyNowModal = ({ object }) => {
     )
 }
 
+const StyledTitle = styled.h1`
+    color:#0d47a1;
+    margin-top:2vh;
+    margin-bottom: 2vh;
+    font-size: 2vw;
+`
+const Wrapper = styled.div`
+    width:30vw;
+    height: 50vh;
+`
+const Container = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 2vw;
+    margin-bottom: 2vh;
+`
+const StyledLabel = styled.label`
+    font-size: large;
+    margin-bottom: 1vh;
+`
+
+const StyledInput = styled.input`
+    color:grey;
+    border:solid 1px;
+    padding-top:0.2vh;
+    padding-bottom:0.2vh;
+    width: 25.3vw;
+`
+const InputContainer2 = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    margin-bottom: 1vh;
+`
+
+const StyledInput1 = styled.input`
+    color:gray;
+    width: 11.5vw;
+    border:solid 1px;
+    padding-top:0.2vh;
+    padding-bottom:0.2vh;
+`
+
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding-top: 3vh;
+`
+const StyledConfirmButton = styled.button`
+    background-color: #fcb900;
+    border-width: 0;
+    border-radius: 2vw;
+    margin-right: 2vw;
+    padding:1vh 2vw;
+    color:white;
+    font-weight: bold;
+    &:hover{
+        cursor: pointer;
+    }
+`
+const StyledCancelButton = styled.button`
+    background-color: #d32f2f;
+    border-width: 0;
+    border-radius: 2vw;
+    margin-left: 2vw;
+    padding:1vh 2vw;
+    color:white;
+    font-weight: bold;
+    &:hover{
+        cursor: pointer;
+    }
+`
+
+const StyledCloseButton = styled.button`
+    background-color: #0d47a1;
+    border: none;
+    border-radius: 5px;
+    width: 80px;
+    color:white;
+    margin-top: 10px;
+    font-size: 18px;
+    &:hover{
+        cursor: pointer;
+    }
+`
+const ConfirmedDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .closeButton{
+        background-color: #d32f2f;
+    }
+`
+
+const StyledButton = styled.button`
+width: 5vw;
+height: 3vh;
+font-size: 15px;
+border:none;
+color: #F9F7F7;
+border-radius: 5px;
+font-weight: 2px;
+background-color: #3F72AF;
+&:hover {
+    opacity: 0.7;
+}
+
+&:active {
+    transform: translateY(2px);
+    }
+`
 export default BuyNowModal;
